f27e78cc057892c008ba91ef5d99b0e2f73404d5
D-Svetlichniy <svetlychnyi.dmytro@gmail.com>
2024-09-18 11:30:23 +0300

    Update demo.py

--- Lab1/demo.py
+++ Lab1/demo.py
@@ -1,9 +1,13 @@
-from Lab1.StorageManager import user1_storage, user2_storage
+from Lab1.StorageManager import StorageManager
 
-# Для користувача "user1" використовується локальне сховище
-user1_storage.upload_file("example.txt", b"Hello, World!")
-user1_storage.download_file("example.txt")
+manager = StorageManager()
 
-# Для користувача "user2" використовується сховище Amazon S3
-user2_storage.upload_file("example.txt", b"Hello, from S3!")
-user2_storage.download_file("example.txt")
+# Для користувача "user1" використовуємо локальне сховище
+user1_storage = manager.get_storage("user1", "local")
+user1_storage.upload_file("example1.txt", b"Hello, Local World!")
+user1_storage.download_file("example1.txt")
+
+# Для користувача "user2" використовуємо сховище Amazon S3
+user2_storage = manager.get_storage("user2", "s3")
+user2_storage.upload_file("example2.txt", b"Hello, S3 World!")
+user2_storage.download_file("example2.txt")
